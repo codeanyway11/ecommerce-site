25d556dc360009c9651f891ee0366b943d0332d5
Charu Agarwal <c_agarwal@ymail.com>
2017-10-30 16:18:58 +0530

    User Registration added

--- admin/users.php
+++ admin/users.php
@@ -17,37 +17,118 @@ if(isset($_GET['delete'])){
     $_SESSION['success_flash'] ="User has been deleted";
     header('Location: users.php');
 }
+if(isset($_GET['add'])){
+    $name=((isset($_POST['name']))?$_POST['name']:'');
+    $email=((isset($_POST['email']))?$_POST['email']:'');
+    $password=((isset($_POST['password']))?$_POST['password']:'');
+    $confirm=((isset($_POST['confirm']))?$_POST['confirm']:'');
+    $permissions=((isset($_POST['permissions']))?$_POST['permissions']:'');
 
-$usersQuery = $db->query("SELECT * FROM users ORDER by full_name");
-?>
-<h2>Users</h2><hr>
-<a href="users.php?add=1" class="btn btn-success pull-right" id="add-product-button">Add New User</a>
-<table class="table table-bordered table-striped table-condensed">
-    <thead>
-        <th></th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Join Date</th>
-        <th>Last Login</th>
-        <th>Permissions</th>
-    </thead>
-    <tbody>
-        <?php while($user = mysqli_fetch_assoc($usersQuery)): ?>
-            <tr>
-                <td>
-                    <?php if($user['id'] != $user_data['id']): ?>
-                        <a href="users.php?delete=<?=$user['id']?>" class="btn btn-default btn-xs" ><span class="glyphicon glyphicon-remove-sign"></span></a>
-                    <?php endif; ?>
-                </td>
-                <td><?=$user['full_name'];?></td>
-                <td><?=$user['email'];?></td>
-                <td><?=pretty_date($user['join_date']);?></td>
-                <td><?=pretty_date($user['last_login']);?></td>
-                <td><?=pretty_permissions($user['permissions']);?></td>
-            </tr>
-        <?php endwhile; ?>
-    </tbody>
-</table>
-<?php
+    $errors = array();
+    if($_POST){
+        $emailQuery = $db->query("SELECT * FROM users WHERE email = '$email'");
+        $emailCount = mysqli_num_rows($emailQuery);
+        if($emailCount > 0){
+            $errors[] = "This email already exists";
+        }
+        $required = array('name', 'email', 'password', 'confirm', 'permissions');
+        foreach($required as $f){
+            if(empty($_POST[$f])){
+                $errors[] ="You must fill out all fields!";
+                break;
+            }
+        }
+
+        if(strlen($password) < 6){
+            $errors[] ="Password must be min 6 characters long.";
+        }
+        if($password !=$confirm){
+            $errors[]="Your passwords do not match!";
+        }
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errors[] ="It must be a valid email address";
+        }
+
+        if(!empty($errors)){
+            display_errors($errors);
+        }else{
+            $hashed = password_hash($password, PASSWORD_DEFAULT);
+            $db->query("INSERT INTO users (full_name, email, password, permissions) VALUES('$name', '$email', '$hashed','$permissions')");
+            $_SESSION['success_flash'] = 'User has been added!';
+            header('Location: users.php');
+        }
+    }
+    ?>
+    <h2 class="text-center">Add A New User</h2><hr>
+    <form class="" action="users.php?add=1" method="post">
+        <div class="form-group col-md-6">
+            <label for="name">Full Name :</label>
+            <input type="text" name="name" id="name" class="form-control" value="<?=$name;?>">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="email">Email :</label>
+            <input type="text" name="email" id="email" class="form-control" value="<?=$email;?>">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="password">Password :</label>
+            <input type="password" name="password" id="password" class="form-control" value="<?=$password;?>">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="confirm">Confirm Password :</label>
+            <input type="password" name="confirm" id="confirm" class="form-control" value="<?=$confirm;?>">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="permissions">Permissions :</label>
+            <select class="form-control" name="permissions">
+                <option value="" <?=(($permissions == '')?' selected':'');?>></option>
+                <option value="editor" <?=(($permissions == 'editor')?' selected':'');?>>Editor</option>
+                <option value="admin,editor" <?=(($permissions == 'admin,editor')?' selected':'');?>>Admin</option>
+            </select>
+        </div>
+        <div class="form-group col-md-6 text-right" style="margin-top:25px;">
+            <a href="users.php" class="btn btn-default">Cancel</a>
+            <input type="submit" value="Add User" class="btn btn-primary">
+        </div>
+
+
+
+    </form>
+
+    <?php
+}else{
+    $usersQuery = $db->query("SELECT * FROM users ORDER by full_name");
+
+
+    ?>
+    <h2>Users</h2><hr>
+    <a href="users.php?add=1" class="btn btn-success pull-right" id="add-product-button">Add New User</a>
+    <table class="table table-bordered table-striped table-condensed">
+        <thead>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Join Date</th>
+            <th>Last Login</th>
+            <th>Permissions</th>
+        </thead>
+        <tbody>
+            <?php while($user = mysqli_fetch_assoc($usersQuery)): ?>
+                <tr>
+                    <td>
+                        <?php if($user['id'] != $user_data['id']): ?>
+                            <a href="users.php?delete=<?=$user['id']?>" class="btn btn-default btn-xs" ><span class="glyphicon glyphicon-remove-sign"></span></a>
+                        <?php endif; ?>
+                    </td>
+                    <td><?=$user['full_name'];?></td>
+                    <td><?=$user['email'];?></td>
+                    <td><?=pretty_date($user['join_date']);?></td>
+                    <td><?=(($user['last_login']==null)?'Never':pretty_date($user['last_login']));?></td>
+                    <td><?=pretty_permissions($user['permissions']);?></td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+<?php }
 include 'includes/footer.php';
 ?>
